95d99bf278aef5129f74bf5ef119465e1898c73d
Paulina Janoszczyk <paulina.janoszczyk@student.htw-berlin.de>
2024-06-26 12:30:24 +0200

    Fix path for AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>About Us</h1>
+    <p>This is an about page for our Vue.js application.</p>Fix path for AboutView.vue
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AboutView'
+});
+</script>
+
+<style scoped>
+.about {
+  margin: 20px;
+}
+</style>
