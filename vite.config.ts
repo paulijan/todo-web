d811bdddbc5ad97068cdcba727d1ba4567ea6554
Paulina Janoszczyk <paulina.janoszczyk@student.htw-berlin.de>
2024-06-30 18:32:44 +0200

    adapt main.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
+const path = require('path')
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
   ],
+  root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
